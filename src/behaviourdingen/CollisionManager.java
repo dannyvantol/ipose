70915f3617f22c86d6b2b2690cb3a0b2ad3eb749
s1108549 <s1108549@student.hsleiden.nl>
2019-03-19 21:24:58 +0100

    gameloader toegevoegd

---
+++ src/behaviourdingen/CollisionManager.java
@@ -0,0 +1,12 @@
+package behaviourdingen;
+
+import behaviourdingen.BehaviourManager;
+import gamedingen.Element;
+
+public class CollisionManager implements BehaviourManager {
+
+    @Override
+    public void handleBehaviour(Element element) {
+
+    }
+}
---
+++ src/behaviourdingen/EventManager.java
@@ -0,0 +1,11 @@
+package behaviourdingen;
+
+import gamedingen.Element;
+
+public class EventManager implements BehaviourManager {
+
+    @Override
+    public void handleBehaviour(Element element) {
+
+    }
+}
