65537707f524d9f6b0c97b2407088ce3df8aa068
boy <boy.bouwense@gmail.com>
2019-03-20 23:18:56 +0100

    EventManager done

--- src/gamedingen/Element.java
+++ src/gamedingen/Element.java
@@ -1,8 +1,12 @@
 package gamedingen;
 
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public abstract class Element extends ImageView {
 
-
+    public Element(String imagePath){
+        super();
+        super.setImage(new Image(imagePath));
+    }
 }
