3c5ea5c6bbdfd1dec61e6cbbac6f91a67fcd28e5
s1108549 <s1108549@student.hsleiden.nl>
2019-03-21 17:46:25 +0100

    Collision manager code

--- src/behaviourdingen/CollisionManager.java
+++ src/behaviourdingen/CollisionManager.java
@@ -1,12 +1,25 @@
 package behaviourdingen;
 
-import behaviourdingen.BehaviourManager;
+import behaviourdingen.behaviors.Collidable;
 import gamedingen.Element;
+import javafx.geometry.Bounds;
+
+import java.util.ArrayList;
 
 public class CollisionManager implements BehaviourManager {
 
+    private ArrayList<Element> objects;
+
+    public CollisionManager(ArrayList<Element> elements) {
+        this.objects = elements;
+    }
+
     @Override
     public void handleBehaviour(Element element) {
-
+        for (Element object : objects) {
+            Bounds bounds = object.getLayoutBounds();
+            if (element.intersects(bounds))
+                ((Collidable) element).handleCollide((Collidable) element, (Collidable) object);
+        }
     }
 }
