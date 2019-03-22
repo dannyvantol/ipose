6bcd83c6dc845985656011fc2f2df9a3d0d13458
boy <boy.bouwense@gmail.com>
2019-03-22 11:54:11 +0100

    EventManager done

--- src/behaviourdingen/behaviors/KeyPressed.java
+++ src/behaviourdingen/behaviors/KeyPressed.java
@@ -2,5 +2,9 @@ package behaviourdingen.behaviors;
 
 import behaviourdingen.Behavior;
 
+import java.util.ArrayList;
+
 public interface KeyPressed extends Behavior {
+
+    public void handleKeyPresses(ArrayList<String> keyCodes);
 }
