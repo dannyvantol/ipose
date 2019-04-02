b9a566319771e4515b5ff69126678c9d5afe34da
boy <boy.bouwense@gmail.com>
2019-04-02 14:01:33 +0200

    collision manager beter gemaakt

--- src/behavior/CollisionManager.java
+++ src/behavior/CollisionManager.java
@@ -10,25 +10,32 @@ import java.util.Arrays;
 
 public class CollisionManager implements BehaviorManager {
 
-    private ArrayList<Element> objects;
+    private ArrayList<Element> elements;
+    private Tile[][] tiles;
 
     public CollisionManager(ArrayList<Element> elements, Tile[][] tiles)
     {
-        this.objects = new ArrayList<Element>();
-        objects.addAll(elements);
-        for (Tile[] tile : tiles) {
-            this.objects.addAll(Arrays.asList(tile));
-        }
+        this.elements = elements;
+        this.tiles = tiles;
 
     }
 
     @Override
     public void handle(Element element) {
-        for (Element object : objects) {
+        for (Element object : elements) {
             if (object instanceof Collidable) {
                 Bounds bounds = object.getLayoutBounds();
                 if (element.intersects(bounds))
                     ((Collidable) element).handleCollision((Collidable) object);
+            }
+        }
+        for(int i = 0; i<tiles.length; i++) {
+            for (int j = 0; j < tiles[i].length; j++) {
+                if (tiles[i][j] instanceof Collidable) {
+                    Bounds bounds = tiles[i][j].getLayoutBounds();
+                    if (element.intersects(bounds))
+                        ((Collidable) element).handleCollision((Collidable) tiles[i][j]);
+                }
             }
         }
     }
