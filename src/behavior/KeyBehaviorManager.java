39527f4db1c367a568991e90b7b9a70925587464
Wesley Klop <wesley19097@gmail.com>
2019-04-11 17:57:44 +0200

    Refactor KeyBehavior to use Set instead of List and lambdas

--- src/behavior/KeyBehaviorManager.java
+++ src/behavior/KeyBehaviorManager.java
@@ -3,11 +3,10 @@ package behavior;
 
 import behavior.behaviors.KeyBehavior;
 import game.Element;
-import javafx.event.EventHandler;
-import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Deze class is verantwoordelijk voor het afhandelen van KeyBehavoir.
@@ -15,30 +14,19 @@ import java.util.ArrayList;
 public class KeyBehaviorManager implements BehaviorManager {
 
     private Stage stage;
-    private ArrayList<String> keyCodes;
+    private Set<String> keyCodes;
 
     public KeyBehaviorManager(Stage stage) {
         this.stage = stage;
-        keyCodes = new ArrayList<>();
+        keyCodes = new HashSet<>();
         setupKeyConfig();
     }
 
     private void setupKeyConfig() {
-        this.stage.getScene().setOnKeyPressed(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent event) {
-                if(!keyCodes.contains(event.getCode().toString())){
-                    keyCodes.add(event.getCode().toString());
-                }
-
-            }
+        this.stage.getScene().setOnKeyPressed(event -> {
+            keyCodes.add(event.getCode().toString());
         });
-        stage.getScene().setOnKeyReleased(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent event) {
-                keyCodes.remove(event.getCode().toString());
-            }
-        });
+        stage.getScene().setOnKeyReleased(event -> keyCodes.remove(event.getCode().toString()));
     }
 
 
--- src/behavior/behaviors/KeyBehavior.java
+++ src/behavior/behaviors/KeyBehavior.java
@@ -2,7 +2,7 @@ package behavior.behaviors;
 
 import behavior.Behavior;
 
-import java.util.ArrayList;
+import java.util.Set;
 
 /**
  * Een behavoir specifiek voor key gerelateerde events.
@@ -13,5 +13,5 @@ public interface KeyBehavior extends Behavior {
      * Deze methode handeld een key event af.
      * @param keyCodes de keycodes die gebruikt zijn.
      * */
-    void handleKeyPresses(ArrayList<String> keyCodes);
+    void handleKeyPresses(Set<String> keyCodes);
 }
