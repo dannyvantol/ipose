09bc8013e71e01ee3dca3e4569908d2a3b137e4c
Wesley Klop <wesley19097@gmail.com>
2019-04-11 17:58:54 +0200

    Cleanup code

--- src/behavior/KeyBehaviorManager.java
+++ src/behavior/KeyBehaviorManager.java
@@ -3,6 +3,7 @@ package behavior;
 
 import behavior.behaviors.KeyBehavior;
 import game.Element;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.util.HashSet;
@@ -10,30 +11,29 @@ import java.util.Set;
 
 /**
  * Deze class is verantwoordelijk voor het afhandelen van KeyBehavoir.
- * */
+ */
 public class KeyBehaviorManager implements BehaviorManager {
 
-    private Stage stage;
+    private Scene scene;
     private Set<String> keyCodes;
 
     public KeyBehaviorManager(Stage stage) {
-        this.stage = stage;
+        this.scene = stage.getScene();
         keyCodes = new HashSet<>();
         setupKeyConfig();
     }
 
     private void setupKeyConfig() {
-        this.stage.getScene().setOnKeyPressed(event -> {
-            keyCodes.add(event.getCode().toString());
-        });
-        stage.getScene().setOnKeyReleased(event -> keyCodes.remove(event.getCode().toString()));
+        scene.setOnKeyPressed(event -> keyCodes.add(event.getCode().toString()));
+        scene.setOnKeyReleased(event -> keyCodes.remove(event.getCode().toString()));
     }
 
 
     /**
      * Geeft aan dat er een Key event afgehandel moet worden door een element met een KeyBehavior uit het spel.
+     *
      * @param element met KeyBehavior.
-     * */
+     */
     @Override
     public void handle(Element element) {
         KeyBehavior keyBehavior = (KeyBehavior) element;
